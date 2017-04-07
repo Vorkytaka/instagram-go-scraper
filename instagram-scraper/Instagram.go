15d394c97784a74f9d6ab8cff883bfd2aea253dc
SolidlSnake <solidlsnake@yandex.ru>
2017-04-08 00:20:10 +0300

    Change error handling for json function

--- instagram-scraper/Instagram.go
+++ instagram-scraper/Instagram.go
@@ -10,36 +10,39 @@ import (
 
 func GetAccoutByUsername(username string) (account Account) {
 	url := fmt.Sprintf(ACCOUNT_JSON_INFO, username)
-	info := _GetJsonFromUrl(url)
+	info, err := _GetJsonFromUrl(url)
+	if err != nil {
+		log.Fatal(err)
+	}
 	account = GetFromAccountPage(info)
 	return account
 }
 
 func GetMedyaByUrl(url string) (media Media) {
 	url += "?__a=1"
-	info := _GetJsonFromUrl(url)
+	info, err := _GetJsonFromUrl(url)
+	if err != nil {
+		log.Fatal(err)
+	}
 	media = GetFromMediaPage(info)
 	return
 }
 
-func _GetJsonFromUrl(url string) (json_body map[string]interface{}) {
+func _GetJsonFromUrl(url string) (json_body map[string]interface{}, err error) {
 	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if resp.StatusCode == 404 {
-		log.Fatal("Page Not Found, Code 404")
+	if err != nil || resp.StatusCode == 404 {
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	err = json.Unmarshal(body, &json_body)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	return
