979a323dcfc622774db6ace59bec797198bd0a95
SolidlSnake <solidlsnake@yandex.ru>
2017-04-08 00:43:40 +0300

    More sexy testing

--- instagram-scraper/Instagram_test.go
+++ instagram-scraper/Instagram_test.go
@@ -5,15 +5,53 @@ import (
 )
 
 func Test_GetAccoutByUsername(t *testing.T) {
-	account := GetAccoutByUsername("instagram")
-	if account.Username != "instagram" || account.Full_name != "Instagram" {
-		t.Error("Unexpected account info.")
+	for _, test_case := range []struct {
+		username, full_name, id string
+	}{
+		{"instagram", "Instagram", "25025320"},
+		{"solidlsnake", "Konstantin", "248188406"},
+	} {
+		account := GetAccoutByUsername(test_case.username)
+		if account.Username != test_case.username ||
+		   account.Full_name != test_case.full_name ||
+		   account.Id != test_case.id {
+			t.Error("Unexpected account info.")
+		}
 	}
 }
 
 func Test_GetMediaByUrl(t *testing.T) {
 	media := GetMedyaByUrl("https://www.instagram.com/p/ceiqEstT6r/")
-	if media.Code != "ceiqEstT6r" || media.Owner.Username != "solidlsnake" {
+	if media.Code != "ceiqEstT6r" || media.Owner.Username != "solidlsnake" || media.Media_type != "image" {
 		t.Error("Unexpected media info.")
+	}
+
+	media = GetMedyaByUrl("https://www.instagram.com/p/12376OtT5o/")
+	if media.Code != "12376OtT5o" || media.Owner.Username != "solidlsnake" || media.Media_type != "video" {
+		t.Error("Unexpected media info.")
+	}
+
+	for _, test_case := range []struct {
+		url, code, username, media_type string
+	}{
+		{
+			"https://www.instagram.com/p/ceiqEstT6r/",
+			"ceiqEstT6r",
+			"solidlsnake",
+			"image",
+		},
+		{
+			"https://www.instagram.com/p/12376OtT5o/",
+			"12376OtT5o",
+			"solidlsnake",
+			"video",
+		},
+	} {
+		media := GetMedyaByUrl(test_case.url)
+		if media.Code != test_case.code ||
+		   media.Owner.Username != test_case.username ||
+		   media.Media_type != test_case.media_type {
+			t.Error("Unexpected media info.")
+		}
 	}
 }
