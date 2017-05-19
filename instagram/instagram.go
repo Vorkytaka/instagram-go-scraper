9cf0c54f30f19ab650c067d2f4417678a73761e8
SolidlSnake <solidlsnake@yandex.ru>
2017-05-19 20:57:41 +0300

    Func to get data ([]byte) from url, instead of json.

--- instagram/instagram.go
+++ instagram/instagram.go
@@ -274,3 +274,18 @@ func getJSONFromURL(url string) (map[string]interface{}, error) {
 
 	return jsonBody, nil
 }
+
+func getDataFromURL(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode == 404 {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
