882bf90fbf25fb1b78b3a8dcdd01cc09c3bd1c42
SolidlSnake <solidlsnake@yandex.ru>
2017-06-06 19:20:41 +0300

    Func for update account info

--- instagram/instagram_test.go
+++ instagram/instagram_test.go
@@ -357,7 +357,7 @@ func Test_SearchForUsers(t *testing.T) {
 	}
 }
 
-func Test_MediaUpdateTest(t *testing.T) {
+func Test_MediaUpdate(t *testing.T) {
 	for _, testCase := range []struct {
 		code string
 	}{
@@ -366,11 +366,32 @@ func Test_MediaUpdateTest(t *testing.T) {
 	} {
 		media := Media{}
 		media.Code = testCase.code
-		media.Update()
+		err := media.Update()
+		if err != nil {
+			t.Error(err)
+		}
 		if len(media.Caption) == 0 ||
 			media.Date == 0 || len(media.ID) == 0 || media.LikesCount == 0 ||
 			len(media.Type) == 0 || len(media.MediaURL) == 0 {
 			t.Error("Media wasn't update")
+		}
+	}
+}
+
+func Test_AccountUpdate(t *testing.T) {
+	for _, testCase := range []struct {
+		username, fullname string
+	}{
+		{"solidlsnake", "Konstantin"},
+	} {
+		account := Account{}
+		account.Username = testCase.username
+		err := account.Update()
+		if err != nil {
+			t.Error(err)
+		}
+		if account.FullName != testCase.fullname {
+			t.Error("Account didn't update")
 		}
 	}
 }
